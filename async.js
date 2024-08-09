163d316ea2dbe594c3e4d08df8d73de62637117f
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-08-09 15:39:44 +0600

    promise, async await updated

--- async.js
+++ async.js
@@ -13,28 +13,24 @@ fetch(" ")
 
 async function getPosts ()
 {
-  try {
+ 
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
    
     const data = await response.json();
     console.log("Fetched posts:", data);
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-  }
+ 
 }
 getPosts();
 
 
 // Arrow function
 const getPosts = async () => {
-  try {
+  
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
    
     const data = await response.json();
     console.log("Fetched posts:", data);
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-  }
+  
 };
 getPosts();
 
